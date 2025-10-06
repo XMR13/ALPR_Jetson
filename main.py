0c862531e835b96a293346677dc55e10dadcf329
Rozen <rizieqmuhammad.13@gmail.com>
2025-10-06 14:47:04 +0700

    commit 2: Mengubah main.py untuk mengetes fungsionalitas dari library agar memastikan bisa atau tidak

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import numpy as np
+import pandas as pd
+
+def testing_library():
+    """Mengetes library apakah bisa atau tidak"""
+    xx = np.array([1,2,3,4,5,6,7])
+    print(F"arraynya adalah {xx}")
+
+    #mengetes untuk bagian pandas
+    print(f"versi library pandas adalah {pd.__version__}")
+
 def main():
     print("Hello from alpr-jetson!")
+    testing_library()
 
 
 if __name__ == "__main__":
